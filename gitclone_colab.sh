b888b4ea893362cd732cd9cc66d4c20c300882df
Nikhil Kamath B <nikhilkb98@gmail.com>
2024-01-17 21:22:29 -0800

    Updated git clone colab script

--- gitclone_colab.sh
+++ gitclone_colab.sh
@@ -1,8 +1,3 @@
-#! /usr/bin/env sh
-
-# Mount Google Drive
-python -c "from google.colab import drive; drive.mount('/content/drive')"
-
 # Clone repository
 _git_repo_url="https://github.com/NikhilKamathB/Code-Mixed-Machine-Translation.git"
 cd "/content/drive/My Drive/colab"
